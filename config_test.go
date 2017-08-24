14634fb86370219346c44923f66a5529059b36b9
Chris Cooney <chris_cooney@hotmail.co.uk>
2017-08-24 21:17:58 +0100

    Fixed config to remove dead params. Added tests for reading in config

--- config_test.go
+++ config_test.go
@@ -4,6 +4,33 @@ import (
   "testing"
 )
 
+func TestGetConfig(t *testing.T) {
+  t.Run("Test ability to load in correct config", func(t *testing.T) {
+    configPath := "examples/config.json"
+    config := GetConfig(configPath)
+
+    if config == nil {
+      t.Error("Expected Config to be parsed. Got nil")
+    }
+
+    if len(config.Endpoints) == 0 {
+      t.Error("Expected Config to contain endpoints. Endpoints length = 0")
+    }
+
+    endpoint := config.Endpoints[0]
+
+    if endpoint.Name != "QA List orders Endpoint" {
+      t.Errorf("Incorrect name for first endpoint. Expected QA List Orders Endpoint. Got %s", endpoint.Name)
+    }
+
+    attack := endpoint.Attacks[0]
+
+    if attack.Type != "CORRUPT_HTTP" {
+      t.Errorf("Expected attack to be CORRUPT_HTTP. Got %s", attack.Type)
+    }
+  })
+}
+
 func TestIsValidConfig(t *testing.T) {
   t.Run("Test ability to check for config errors", func(t *testing.T) {
     config := &Config{}
